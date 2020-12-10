2ee54e17ecc04e3d50ae90a4cce410ff217ef9c1
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-12-10 10:18:06 +0530

    [Shubham]UC1: created controller

---
+++ src/main/java/com/capgi/employeepayrollspring/model/EmployeePayrollData.java
@@ -0,0 +1,50 @@
+package com.capgi.employeepayrollspring.model;
+
+import com.capgi.employeepayrollspring.dto.EmployeePayrollDTO;
+
+public class EmployeePayrollData {
+	private int employeeId;
+	private String name;
+	private long salary;
+
+	public EmployeePayrollData(int employeeId, String name, long salary) {
+		super();
+		this.employeeId = employeeId;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public EmployeePayrollData() {
+	}
+
+	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
+		this.setEmployeeId(empId);
+		this.setName(empPayrollDTO.name);
+		this.setSalary(empPayrollDTO.salary);
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
+}
---
+++ src/main/java/com/capgi/employeepayrollspring/service/EmployeePayrollService.java
@@ -0,0 +1,43 @@
+package com.capgi.employeepayrollspring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.capgi.employeepayrollspring.dto.EmployeePayrollDTO;
+import com.capgi.employeepayrollspring.model.EmployeePayrollData;
+
+@Service
+public class EmployeePayrollService {
+
+	public List<EmployeePayrollData> getEmployeePayrollData() {
+		List<EmployeePayrollData> empDataList = new ArrayList<>();
+		empDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("pankay", 30000)));
+
+		return empDataList;
+	}
+
+	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+		EmployeePayrollData empData = null;
+		empData = new EmployeePayrollData(1, new EmployeePayrollDTO("pankaj", 30000));
+		return empData;
+	}
+
+	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData empData = null;
+		empData = new EmployeePayrollData(1, empPayrollDTO);
+		return empData;
+	}
+
+	public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData empData = null;
+		empData = new EmployeePayrollData(1, empPayrollDTO);
+		return empData;
+	}
+
+	public void deleteEmployeePayrollData(int empId) {
+
+	}
+
+}
