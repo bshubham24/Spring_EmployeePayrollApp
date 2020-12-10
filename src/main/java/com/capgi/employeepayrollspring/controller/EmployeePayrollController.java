e65ee3b567ce813466ab66ef4d7f548ece2a681e
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-12-10 12:54:08 +0530

    [Shubham]Added: service layer in employee payroll app

--- src/main/java/com/capgi/employeepayrollspring/controller/EmployeePayrollController.java
+++ src/main/java/com/capgi/employeepayrollspring/controller/EmployeePayrollController.java
@@ -1,5 +1,8 @@
 package com.capgi.employeepayrollspring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,23 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capgi.employeepayrollspring.dto.EmployeePayrollDTO;
 import com.capgi.employeepayrollspring.dto.ResponseDTO;
 import com.capgi.employeepayrollspring.model.EmployeePayrollData;
+import com.capgi.employeepayrollspring.service.IEmployeePayrollService;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
 
+	@Autowired
+	private IEmployeePayrollService employeePayrollService;
+
 	@RequestMapping(value = { "", "/", "/get" })
 	public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, new EmployeePayrollDTO("pankaj", 30000));
-		ResponseDTO respDTO = new ResponseDTO("Get call Success", empData);
+		List<EmployeePayrollData> empPayrollList = null;
+		empPayrollList = employeePayrollService.getEmployeePayrollData();
+		ResponseDTO respDTO = new ResponseDTO("Get call Success", empPayrollList);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 	@RequestMapping("/get/{empId}")
 	public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(empId, new EmployeePayrollDTO("pankaj", 30000));
+		empData = employeePayrollService.getEmployeePayrollDataById(empId);
 		ResponseDTO respDTO = new ResponseDTO("Get call for id: " + empId + " Successfull", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
@@ -37,7 +44,7 @@ public class EmployeePayrollController {
 	@PostMapping("/create")
 	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, employeePayrollDTO);
+		empData = employeePayrollService.createEmployeePayrollData(employeePayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Created Employee Payroll Data Successfully", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
@@ -45,13 +52,14 @@ public class EmployeePayrollController {
 	@PutMapping("/update")
 	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, employeePayrollDTO);
+		empData = employeePayrollService.updateEmployeePayrollData(employeePayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete/{empId}")
 	public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+		employeePayrollService.deleteEmployeePayrollData(empId);
 		ResponseDTO respDTO = new ResponseDTO("Deleted Employee Payroll Data Successfully", "Deleted id: " + empId);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
--- src/main/java/com/capgi/employeepayrollspring/service/IEmployeePayrollService.java
+++ src/main/java/com/capgi/employeepayrollspring/service/IEmployeePayrollService.java
@@ -6,13 +6,13 @@ import com.capgi.employeepayrollspring.dto.EmployeePayrollDTO;
 import com.capgi.employeepayrollspring.model.EmployeePayrollData;
 
 public interface IEmployeePayrollService {
-	List<EmployeePayrollData> getEmployeeData();
+	List<EmployeePayrollData> getEmployeePayrollData();
 
-	EmployeePayrollData getEmployeePayrollById(int empId);
+	EmployeePayrollData getEmployeePayrollDataById(int empId);
 
 	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
 
-	EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO);
+	EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
 
 	void deleteEmployeePayrollData(int empId);
 }
