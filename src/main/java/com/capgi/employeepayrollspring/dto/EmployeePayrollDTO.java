9ff386bed3abf2a57bef0c865fe8948dc09d988c
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-12-14 14:51:24 +0530

    [Shubham]Added: lombok, loggers and environment varible for database

--- src/main/java/com/capgi/employeepayrollspring/dto/EmployeePayrollDTO.java
+++ src/main/java/com/capgi/employeepayrollspring/dto/EmployeePayrollDTO.java
@@ -1,8 +1,6 @@
 package com.capgi.employeepayrollspring.dto;
 
 import java.sql.Date;
-import java.time.LocalDate;
-import java.util.regex.Pattern;
 
 import javax.persistence.Id;
 
@@ -35,15 +33,15 @@ public class EmployeePayrollDTO {
 	}
 
 	public EmployeePayrollDTO(EmployeePayrollDB user) {
-		if (validate(name, salary, gender, department, startDate)) {
-			this.id = user.getId();
-			this.name = user.getName();
-			this.salary = user.getSalary();
-			this.gender = user.getGender();
-			this.department = user.getDepartment();
-			this.startDate = user.getStartDate();
-			this.notes = user.getNotes();
-		}
+		// if (validate(name, salary, gender, department, startDate)) {
+		this.id = user.getId();
+		this.name = user.getName();
+		this.salary = user.getSalary();
+		this.gender = user.getGender();
+		this.department = user.getDepartment();
+		this.startDate = user.getStartDate();
+		this.notes = user.getNotes();
+		// }
 	}
 
 	final static String NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
@@ -51,28 +49,28 @@ public class EmployeePayrollDTO {
 	public EmployeePayrollDTO(String name, long salary, String gender, String department, Date startDate,
 			String notes) {
 
-		if (validate(name, salary, gender, department, startDate)) {
-			this.name = name;
-			this.salary = salary;
-			this.gender = gender;
-			this.department = department;
-			this.startDate = startDate;
-			this.notes = notes;
-		}
+		// if (validate(name, salary, gender, department, startDate)) {
+		this.name = name;
+		this.salary = salary;
+		this.gender = gender;
+		this.department = department;
+		this.startDate = startDate;
+		this.notes = notes;
+		// }
 
 	}
 
-	public static boolean validate(String name, long salary, String gender, String department, Date startDate) {
-		boolean b1 = Pattern.matches(NAME_PATTERN, name);
-		boolean b2 = (salary >= 100000 && salary <= 500000) ? true : false;
-		boolean b3 = (gender.equalsIgnoreCase("female") || gender.equalsIgnoreCase("male")) ? true : false;
-		boolean b4 = department.contains("HR") || department.contains("sales") || department.contains("Engineering")
-				|| department.contains("Marketing");
-		Date current = Date.valueOf(LocalDate.now());
-		boolean b5 = (startDate.before(current)) ? true : false;
-
-		return b1 && b2 && b3 && b4 && b5;
-	}
+//	public static boolean validate(String name, long salary, String gender, String department, Date startDate) {
+//		boolean b1 = Pattern.matches(NAME_PATTERN, name);
+//		boolean b2 = (salary >= 100000 && salary <= 500000) ? true : false;
+//		boolean b3 = (gender.equalsIgnoreCase("female") || gender.equalsIgnoreCase("male")) ? true : false;
+//		boolean b4 = department.contains("HR") || department.contains("sales") || department.contains("Engineering")
+//				|| department.contains("Marketing");
+//		Date current = Date.valueOf(LocalDate.now());
+//		boolean b5 = (startDate.before(current)) ? true : false;
+//
+//		return b1 && b2 && b3 && b4 && b5;
+//	}
 
 	public Long getId() {
 		return id;
